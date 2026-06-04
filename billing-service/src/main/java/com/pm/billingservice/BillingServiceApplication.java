8bf88cd2d3f6ebbe7bcac3176b535e6474559438
Ayushman Bhattacharya <ayushman.rick007@gmail.com>
2026-06-04 15:51:38 +0530

    intial commit

--- billing-service/src/main/java/com/pm/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/com/pm/billingservice/BillingServiceApplication.java
@@ -2,7 +2,7 @@ package com.pm.billingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//done
 @SpringBootApplication
 public class BillingServiceApplication {
 
